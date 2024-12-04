71a613c3614c71994c2729dc9f0fbcf7d4f10d65
elianaiasi <>
2024-12-04 16:29:58 -0300

    elianaiasi
    new commit
    
    Co-Authored-By: Elianaia Lima Costa de Amorim <elianaia7@gmail.com>

---
+++ todolist/src/test/java/br/com/elianaia/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elianaia.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
